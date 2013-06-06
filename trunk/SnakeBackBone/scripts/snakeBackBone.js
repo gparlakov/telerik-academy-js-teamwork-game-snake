97259574030801b501253bdffb48dbb707435b9d
gparlakov <gparlakov@5311d87f-156c-57fd-77dc-1822cb9fd504>
2013-06-06 19:49:49 +0000

    Starting code - the three main NameSpaces. In one file for the moment

---
+++ trunk/SnakeBackBone/scripts/snakeBackBone.js
@@ -0,0 +1,76 @@
+﻿//**using jQuery $**///
+//using prototype OOP ... or the other one//
+var Coords = {
+    init : function (posX, poxY) {
+        this.X = posX;
+        this.Y = posY;
+    }        
+}
+
+var GameObjectsNS = (function () {   
+
+    var GameObject = {
+        init: function (positionX, positionY, width) {
+
+        },
+        getPosition: function () {
+            return 
+        }
+    }
+
+    var StaticObject = {
+        init: function (positionX,  positionY, width, collisionGroup) {
+            this._super.init.apply(this, arguments);
+            this.collisionGroup = collisionGroup;
+        }
+    }
+    StaticObject.inherit(GameObject);
+
+    var MovableObject = {
+        init
+    }
+
+    return {
+        staticObject: StaticObject,
+        movableObject: MovableObject,
+        snakeObject: SnakeObject,
+        gameObjectsFactory: GameObjectsFactory
+    }
+})();
+
+var GameControllersNS = (function () {
+
+    var GameController = {
+        init: function (drawer, gameObjectsFactory) {
+
+        },
+        initEvents: function (){
+
+        }
+
+    }
+
+
+    return {
+        gameController: GameController,
+        menuController: MenuController,
+    }
+
+})();
+
+var DrawersNS = (function () {
+    var CanvasDrawer = {
+        init: function (cavasElement) {
+
+        },
+        draw : function() {
+
+        }
+
+    }
+
+    return {
+        canvasDrawer: CanvasDrawer,
+        menuDrawer: MenuDrawer
+    }
+})();
