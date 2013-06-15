99cff86fda1a32497399cb130afb19cefb2618c1
adrian.guliashki@gmail.com <adrian.guliashki@gmail.com@5311d87f-156c-57fd-77dc-1822cb9fd504>
2013-06-15 09:31:37 +0000

    Coords unit test method initialize ready.

--- trunk/UnitTestGameObject/script/snake-gameobject-test.js
+++ trunk/UnitTestGameObject/script/snake-gameobject-test.js
@@ -1,4 +1,4 @@
-﻿module('Coords.init');
+﻿module('Coords.initialize');
 test('When game object is created, should set correct values', function () {
     var posX = 10;
     var posY = 15;
@@ -6,3 +6,33 @@ test('When game object is created, should set correct values', function () {
     equal(position.X, posX);
     equal(position.Y, posY);
 });
+
+module('Coords.equals');
+test('Check 2 same Coords objects if equal', function () {
+    var firstPosX = 10;
+    var firstPosY = 25;
+    var firstPos = new Coords(firstPosX, firstPosY);
+
+    var secondPosX = 10;
+    var secondPosY = 25;
+    var secondPos = new Coords(secondPosX, secondPosY);
+
+    var expected = true;
+    var actual = firstPos.equals(secondPos);
+    equal(expected, actual);
+});
+
+test('Check 2 different Coords object', function () {
+    var firstPosX = 10;
+    var firstPosY = 25;
+    var firstPos = new Coords(firstPosX, firstPosY);
+
+    var secondPosX = 10;
+    var secondPosY = 15;
+    var secondPos = new Coords(secondPosX, secondPosY);
+
+    var expected = false;
+    var actual = firstPos.equals(secondPos);
+    equal(expected, actual);
+});
+
