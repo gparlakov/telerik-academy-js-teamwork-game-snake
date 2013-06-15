24ce4db3849d142e9d5f7f95fa3b8766d9a78eda
adrian.guliashki@gmail.com <adrian.guliashki@gmail.com@5311d87f-156c-57fd-77dc-1822cb9fd504>
2013-06-15 09:12:54 +0000

    Unit test game object started

---
+++ trunk/UnitTestGameObject/script/snake-gameobject-test.js
@@ -0,0 +1,8 @@
+﻿module('Coords.init');
+test('When game object is created, should set correct values', function () {
+    var posX = 10;
+    var posY = 15;
+    var position = new Coords(posX, posY);
+    equal(position.X, posX);
+    equal(position.Y, posY);
+});
